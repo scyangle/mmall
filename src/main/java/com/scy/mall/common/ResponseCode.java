774deed20f5b6932c9b636cbd19c4bc2f846cf8f
Scyangle <scyangle1314@163.com>
2017-07-31 20:21:23 +0800

    update the ServerResponse Function

--- src/main/java/com/scy/mall/common/ResponseCode.java
+++ src/main/java/com/scy/mall/common/ResponseCode.java
@@ -4,9 +4,10 @@ package com.scy.mall.common;
  * Created by Shichengyao on 2017/7/31.
  */
 public enum ResponseCode {
-    SUCCESS(0,"SUCCESS"),ERROR(1,"ERROR");
+    SUCCESS(0, "SUCCESS"), ERROR(1, "ERROR"), ILLEGAL_ARGUMENT(2, "ILLEGAL_ARGUMENT"), NEED_LOGIN(10, "NEED_LOGIN");
     private final int code;
     private final String desc;
+
     ResponseCode(int code, String desc) {
         this.code = code;
         this.desc = desc;
