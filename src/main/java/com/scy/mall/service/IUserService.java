66b7ee04b42902ce5c75622eab264dbda359047c
Scyangle <scyangle1314@163.com>
2017-07-31 10:10:18 +0800

    add IUserService

---
+++ src/main/java/com/scy/mall/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.scy.mall.service;
+
+import com.scy.mall.pojo.User;
+
+/**
+ * Created by Shichengyao on 2017/7/31.
+ */
+public interface IUserService {
+//    ServiceResponse<User> login(String username, String password);
+}
