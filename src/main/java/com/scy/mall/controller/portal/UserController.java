eca2d3b31b471674f76a65dc10e55259b7705cce
Scyangle <scyangle1314@163.com>
2017-09-23 17:52:52 +0800

    update the project base

--- src/main/java/com/scy/mall/controller/portal/UserController.java
+++ src/main/java/com/scy/mall/controller/portal/UserController.java
@@ -1,7 +1,12 @@
 package com.scy.mall.controller.portal;
 
+import com.scy.mall.common.ServerResponse;
+import com.scy.mall.pojo.User;
+import com.scy.mall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
@@ -12,9 +17,12 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping(value = "/user/")
 public class UserController {
+    @Autowired
+    private IUserService userService;
     @RequestMapping("login.do")
     @ResponseBody
-    public Object login(String username, String password, HttpSession session) {
-        return null;
+    public Object login(@RequestParam(required = true) String username, String password, HttpSession session) {
+        ServerResponse<User> response = userService.login(username, password);
+        return response;
     }
 }
--- src/main/java/com/scy/mall/service/impl/UserServiceImpl.java
+++ src/main/java/com/scy/mall/service/impl/UserServiceImpl.java
@@ -1,15 +1,25 @@
 package com.scy.mall.service.impl;
 
 import com.scy.mall.common.ServerResponse;
+import com.scy.mall.dao.UserMapper;
 import com.scy.mall.pojo.User;
 import com.scy.mall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Shichengyao on 2017/9/23.
  */
+@Service
 public class UserServiceImpl implements IUserService{
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public ServerResponse<User> login(String username, String password) {
+        int count = userMapper.checkUserName(username);
+        if (count != 1) {
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
         return null;
     }
 }
